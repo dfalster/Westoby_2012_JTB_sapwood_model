17d8094e4e10fa56577ae22f073e637cedfffe95
Daniel Falster <daniel.falster@mq.edu.au>
2013-08-30 09:49:45 +0200

    tidy syntax

--- R/functions-model.R
+++ R/functions-model.R
@@ -1,8 +1,7 @@
 
 
-
 load_default_parameters <- function(){
-  
+
   #leaf biochemistry - fixed parameters
   Leaf = list(km= 223 *(1+210000/216000), 	#umol / mol = Kc * (1+oi/Ko)
               gamma=32.6, 		              #umol / mol (= ppm)
@@ -10,7 +9,7 @@ load_default_parameters <- function(){
               kj=2.0*1.0*44.3,       # electron transport rate coefficient ymol/gN/s
               alpha = 0.06,		  #quantum yield
               theta = 0.8)     	#curvature
-  
+
   #Environmental Parameters
   Env = list(
     #Climate variables
@@ -18,28 +17,29 @@ load_default_parameters <- function(){
     ca= 380,				#umol/mol
     psi.soil = -0.5,		#Mpa
     temp= 10, #not actually used anywhere at present
-    
+
     #Soil params - Details on soil hydraulics from Sperry et al 1998. These params for silt loam
     b=4.38, 						#soil texture parameter affects the conductance at a given water pot
     psi.soil.sat= -0.5*0.1^-0.5 /1000,	#Soil water potential under saturating condns, 0.1= GWD = geometric mean particle dimaeter
     K.soil.max=12.1,				#maximum conductance of soil
     dw=0.005)						#Av pathlength for water traveling from soil to root via root induced water potential gradient
-  
+
   Plant =list(
-    
+
     #Leaf parameters
     r.lf=	30,				#resistance in petiole/leaf
     psi.crit= -4,		#MPA
-    LMA=400, #97,					#g/m2;
+    LMA=400, #97,					#g/m2
+
     lls=pred.LL(400),		#yrs
     narea.lf=1.75,			#g/m2
-    
+
     #stem
     K.stem=75,        		#mol m /mPA/m2/s   (note this is per m2 sapwood)
     den.stem=0.6e6,			#g/m3
-    height = 15,			#m; equivilent to pathlength of xylem right now
+    height = 15,			#m  equivilent to pathlength of xylem right now
     nmass.stem=0.001,		#g/g
-    
+
     #root
     kroot = 1e8,			#conductance from root surface to stem base -high so that is negligible
     root.rad=0.001,			#m - root radius
@@ -61,13 +61,15 @@ pred.nmass = function(LMA){
 
 #CO2 (Rubsico) limited photosythesis
 Av=function(n.area, n.alloc, ci, temp, leaf){
-  Vmax =  n.area*n.alloc*leaf$kv;
+  Vmax =  n.area*n.alloc*leaf$kv
+
   return((Vmax*(ci-leaf$gamma))/(ci+leaf$km)- Rd(n.area, temp))
 }
 
 #Electron transport (light) limited photosythesis
 Aj=function(n.area, n.alloc, ci, q, temp, leaf){
-  Jmax =  n.area*(1-n.alloc)*leaf$kj;
+  Jmax =  n.area*(1-n.alloc)*leaf$kj
+
   return (Jmax*(ci-leaf$gamma)/(4*(ci+2*leaf$gamma))- Rd(n.area, temp))
 }
 
@@ -87,8 +89,11 @@ Resp_const <-function(temp, RESP.coeff=0.066){
   resp = RESP.coeff/       #gC /gN / day
     (24*3600)* #convert to per second rate
     (10^6)/12* 	 #convert to umol C02
-    exp(308.56*(1/56.02-1/(temp+46.02))); #temperature adjustment
-  return (resp);}              #umol C02 /gN/ s
+    exp(308.56*(1/56.02-1/(temp+46.02)))
+  #temperature adjustment
+  return (resp)
+  #umol C02 /gN/ s
+  }
 
 #Create a plot of A-Ci curves showing Aj and Av
 Plot.Aci=function(n.area, n.alloc, q, temp, leaf){
@@ -110,9 +115,12 @@ Ksoil<-function(env){
 
 #returns stomatal conductance as function of diff parameters
 stom.func <-function(sa.la, ra.la,plant, env) {
-  r.st <- plant$height/plant$K.stem/sa.la;
-  r.so<- env$dw/Ksoil(env)/ra.la;
-  r.rt<- 1/plant$kroot/ra.la;
+  r.st <- plant$height/plant$K.stem/sa.la
+
+  r.so<- env$dw/Ksoil(env)/ra.la
+
+  r.rt<- 1/plant$kroot/ra.la
+
   #print(c(r.so,r.rt,r.st))
   g<-1/(1.6*env$vpd/100)*(env$psi.soil - plant$psi.crit - plant$height*998.2*9.8/10^6)/(r.so + r.rt + r.st + plant$r.lf)
   return(g)
@@ -124,14 +132,22 @@ ci.AvAj.intersect=function(n.alloc, leaf){
 }
 
 leaf.solve <- function(N, q, gc, ca, temp, leaf){
-  ri = 0.066/(24*3600)/(12*10^(-6))*exp(308.56*(1/56.02-1.0/(temp+46.02))); #daytime respiration parameter need temp
-  kv=leaf$kv; kj=leaf$kj; T=leaf$gamma; Km=leaf$km;
-  soln<-NULL;
+  ri = 0.066/(24*3600)/(12*10^(-6))*exp(308.56*(1/56.02-1.0/(temp+46.02)))
+  #daytime respiration parameter need temp
+  kv=leaf$kv
+  kj=leaf$kj
+  T=leaf$gamma
+  Km=leaf$km
+  soln<-NULL
+
   for(i in 1:length(gc)){		#do for all values of gc if a vector
     n_al<- (4*gc[i]*kj*ca*T-16*gc[i]*kv*T*Km+N*kj^2*T-8*gc[i]*ca*kv*Km+32*gc[i]*kv*T^2+N*kj^2*Km+16*gc[i]*ca*kv*T+4*gc[i]*kj*Km*T			+16*ri*N*kv*T-2*gc[i]*kj*ca*Km-8*ri*N*Km*kv+2*N*kj*Km*kv+4*ri*N*kj*T+8*N*kv*T*kj-2*ri*N*kj*Km-2*gc[i]*kj*Km^2+2*((2*T-Km)^2*(64*gc[i]			^2*kv^2*T^2+8*gc[i]*kv*Km*ri*N*kj+16*gc[i]*ca*kv*ri*N*kj+2*gc[i]*kj^2*Km*ri*N+16*T*gc[i]^2*kv*kj*ca+64*T*gc[i]*kv^2*ri*N+16*T*gc[i]*kv*ri*N*kj
-                                                                                                                                                                                                                                                                 +4*T*gc[i]*kv*N*kj^2+ri^2*N^2*kj^2+16*ri^2*N^2*kv^2+gc[i]^2*kj^2*ca^2+gc[i]^2*kj^2*Km^2+16*gc[i]^2*ca^2*kv^2+N^2*kv^2*kj^2+8*gc[i]			^2*ca^2*kv*kj+2*gc[i]^2*kj^2*ca*Km-2*gc[i]*ca*kv*N*kj^2+2*gc[i]*kv*N*kj^2*Km+8*gc[i]^2*ca*kv*kj*Km-8*gc[i]*ca*kv^2*N*kj+64*T*gc[i]^2*ca*kv^2			+16*T*gc[i]^2*kv*kj*Km+32*T*gc[i]*kv^2*N*kj-2*ri*N^2*kv*kj^2+8*ri^2*N^2*kv*kj-8*ri*N^2*kv^2*kj+2*gc[i]*kj^2*ca*ri*N+32*gc[i]			*ca*kv^2*ri*N))^	(1/2))/N/(kj+4*kv)/(12*kv*T+kj*Km+kj*T);
-    ci<- ci.AvAj.intersect(n_al, leaf);
-    A<-  gc[i]*(ca-ci);
+                                                                                                                                                                                                                                                                 +4*T*gc[i]*kv*N*kj^2+ri^2*N^2*kj^2+16*ri^2*N^2*kv^2+gc[i]^2*kj^2*ca^2+gc[i]^2*kj^2*Km^2+16*gc[i]^2*ca^2*kv^2+N^2*kv^2*kj^2+8*gc[i]			^2*ca^2*kv*kj+2*gc[i]^2*kj^2*ca*Km-2*gc[i]*ca*kv*N*kj^2+2*gc[i]*kv*N*kj^2*Km+8*gc[i]^2*ca*kv*kj*Km-8*gc[i]*ca*kv^2*N*kj+64*T*gc[i]^2*ca*kv^2			+16*T*gc[i]^2*kv*kj*Km+32*T*gc[i]*kv^2*N*kj-2*ri*N^2*kv*kj^2+8*ri^2*N^2*kv*kj-8*ri*N^2*kv^2*kj+2*gc[i]*kj^2*ca*ri*N+32*gc[i]			*ca*kv^2*ri*N))^	(1/2))/N/(kj+4*kv)/(12*kv*T+kj*Km+kj*T)
+
+    ci<- ci.AvAj.intersect(n_al, leaf)
+
+    A<-  gc[i]*(ca-ci)
+
     soln=cbind(soln,rbind(A, ci, n_al))
   }
   return (soln)
@@ -140,48 +156,67 @@ leaf.solve <- function(N, q, gc, ca, temp, leaf){
 
 #Calculate physiological maximum height
 height.max<-function(ra.la, leaf, plant, env){
-  Hmax= uniroot(cost_ben.opt.Cbal.heightwrap, c(1e-2, 150), ra.la=ra.la, leaf=leaf, plant=plant, env=env)$root;
-  plant.temp = plant; plant.temp$height=Hmax;
+  Hmax= uniroot(cost_ben.opt.Cbal.heightwrap, c(1e-2, 150), ra.la=ra.la, leaf=leaf, plant=plant, env=env)$root
+
+  plant.temp = plant
+
+  plant.temp$height=Hmax
+
   return(rbind(cost_ben.opt(ra.la, leaf, plant.temp, env), Hmax))
 }
 
 cost_ben.opt.Cbal.heightwrap <- function(height, ra.la, leaf, plant , env){
-  Plant.temp = plant; Plant.temp$height=height;
+  Plant.temp = plant
+  Plant.temp$height=height
+
   return(cost_ben.opt.Cbal(ra.la, leaf, Plant.temp, env))
 }
 
 cost_ben.opt.Cbal <- function(ra.la, leaf, plant , env, SALA.min = 1E-8, SALA.max = 1E-2){
-  optim<-optimise(net.Cbal, c(SALA.min, SALA.max), maximum= T, tol = 0.000001, ra.la=ra.la, leaf=leaf, plant=plant, env=env);
+  optim<-optimise(net.Cbal, c(SALA.min, SALA.max), maximum= T, tol = 0.000001, ra.la=ra.la, leaf=leaf, plant=plant, env=env)
+
   return(optim$objective)
 }
 
 cost_ben.opt <- function(ra.la, leaf, plant , env, SALA.min = 1E-8, SALA.max = 1E-2){
-  optim<-optimise(net.Cbal, c(SALA.min, SALA.max), maximum= T, tol = 0.000001,  ra.la=ra.la, leaf=leaf, plant=plant, env=env);
-  return(cost_ben(optim$maximum, ra.la, leaf, plant, env));
+  optim<-optimise(net.Cbal, c(SALA.min, SALA.max), maximum= T, tol = 0.000001,  ra.la=ra.la, leaf=leaf, plant=plant, env=env)
+
+  return(cost_ben(optim$maximum, ra.la, leaf, plant, env))
+
 }
 
 cost_ben.opt2D <- function(leaf, plant, env){
-  Rala<-optimise(net.Cbal.2D, c(1e-10, 50), maximum= T, tol = 0.000001, leaf=leaf, plant=plant, env=env);
-  Sala<-cost_ben.opt(ra.la=Rala$maximum, leaf=leaf,plant=plant, env=env);
-  return(rbind(Sala, ra.la= Rala$maximum));
+  Rala<-optimise(net.Cbal.2D, c(1e-10, 50), maximum= T, tol = 0.000001, leaf=leaf, plant=plant, env=env)
+
+  Sala<-cost_ben.opt(ra.la=Rala$maximum, leaf=leaf,plant=plant, env=env)
+
+  return(rbind(Sala, ra.la= Rala$maximum))
+
 }
 
 #used for optimsation
 net.Cbal.2D<-function(ra.la, leaf, plant, env){
-  return(cost_ben.opt.Cbal(ra.la=ra.la, leaf=leaf,plant=plant, env=env));
+  return(cost_ben.opt.Cbal(ra.la=ra.la, leaf=leaf,plant=plant, env=env))
+
 }
 
 #used for optimsation
 net.Cbal<-function(sa.la, ra.la, leaf, plant, env){
-  return(cost_ben(sa.la,ra.la, leaf, plant, env)[8]);
+  return(cost_ben(sa.la,ra.la, leaf, plant, env)[8])
+
 }
 
 cost_ben <- function(sa.la, ra.la, leaf, plant, env){
-  g<-stom.func(sa.la, ra.la,plant, env);    #calculate stomatal conductance as function hydraulic parameters
-  leaf.sol = leaf.solve(plant$narea.lf, env$q, g, env$ca, env$temp, leaf); #solve for oeprating A
-  Rev <- revenue (leaf.sol[1,], sa.la, ra.la, plant, env);
-  Invest<- investment(sa.la, ra.la, plant);
-  Net <- Rev - Invest;
+  g<-stom.func(sa.la, ra.la,plant, env)
+     #calculate stomatal conductance as function hydraulic parameters
+  leaf.sol = leaf.solve(plant$narea.lf, env$q, g, env$ca, env$temp, leaf)
+  #solve for oeprating A
+  Rev <- revenue (leaf.sol[1,], sa.la, ra.la, plant, env)
+
+  Invest<- investment(sa.la, ra.la, plant)
+
+  Net <- Rev - Invest
+
   return(rbind(sa.la, g, leaf.sol, Rev, Invest, Net))
 }
 
@@ -192,7 +227,8 @@ investment <-function(sa.la, ra.la, plant) {
   stem.mass <- sa.la*plant$height  *plant$den.stem        #g/m2  = m2/m2 *m*g/m3
   root.mass<-  ra.la*plant$root.rad*plant$den.root/2
   tot.n = plant$narea.lf+stem.mass*plant$nmass.stem+root.mass*plant$nmass.root #g/m2
-  n.cost=0.0; #set to zero during revision for JTB, 2012.02.27    #9.12*tot.n			      #C cost of N 9.12 gC g-1N for fixers from Gutschick 1981
+  n.cost=0.0
+  #set to zero during revision for JTB, 2012.02.27    #9.12*tot.n			      #C cost of N 9.12 gC g-1N for fixers from Gutschick 1981
   return(0.45*(leaf.mass + stem.mass + root.mass) + n.cost)
 }
 
@@ -201,22 +237,27 @@ revenue <- function(A, sa.la,ra.la, plant, env){
   leaf.ps = A *
     (12*60*60)*   		   		#umol/m2/day   = *[(hrs/day)*(mins/hr)*(sec/min)
     10^(-6)*12*		       		#g/m2/day     = *(mol/umol)*gram C/mol
-    0.5;				      		#discount productivity to account for leaf orientation and leaf ageing
+    0.5
+ 				      		#discount productivity to account for leaf orientation and leaf ageing
   #Respiration
-  stem.n <- sa.la * plant$height * plant$den.stem * plant$nmass.stem;  #gN /m2 leaf
+  stem.n <- sa.la * plant$height * plant$den.stem * plant$nmass.stem
+   #gN /m2 leaf
   stem.resp = stem.n*Resp_const(env$temp)*
     24*3600*                	#s /day
-    12/(10^6); 	          	#g C / umol C02
+    12/(10^6)
+  	          	#g C / umol C02
   #= gC/m2 leaf /day
   leaf.night.resp = plant$narea.lf*Resp_const(env$temp)*   #umol C02 /m2/ s
     12*3600*	            #s /day
-    12/(10^6); 	          #convert umol to g C
+    12/(10^6)
+  	          #convert umol to g C
   #= gC/m2 leaf /day
   root.n <- ra.la * plant$root.rad * plant$den.root/2 * plant$nmass.root
   root.resp = root.n*Resp_const(env$temp)*
     24*3600*                #s /day
-    12/(10^6);
-  
+    12/(10^6)
+
+
   Rev = leaf.ps  - leaf.night.resp - stem.resp -root.resp   #gC/m2/day
   Rev = Rev*(plant$lls*365)	#gC/lls  *[years*(days/year)]
   return(Rev)
