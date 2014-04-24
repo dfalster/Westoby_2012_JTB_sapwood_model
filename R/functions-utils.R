73fdec4af096bb5bbfaa3b596efb65e0d6a7f742
Daniel Falster <daniel.falster@mq.edu.au>
2014-04-24 16:54:13 +1000

    clean whitespace

--- R/functions-utils.R
+++ R/functions-utils.R
@@ -9,11 +9,11 @@ seq_log <- function(lower, upper, multiply) {
     return(temp)
 }
 
-fig.setup <- function(ylim, ylab, y_ax, y_ax2, xlim, xlab, x_ax, x_ax2, cex.A, cex.L, 
+fig.setup <- function(ylim, ylab, y_ax, y_ax2, xlim, xlab, x_ax, x_ax2, cex.A, cex.L,
     log = "") {
-    plot(1:2, 1:2, type = "n", log = log, axes = F, ann = F, xlim = xlim, ylim = ylim, 
+    plot(1:2, 1:2, type = "n", log = log, axes = F, ann = F, xlim = xlim, ylim = ylim,
         xaxs = "i", yaxs = "i", las = 1)
-    
+
     axis(2, at = y_ax, labels = y_ax, las = 1, tck = 0.03, cex.axis = cex.A, adj = 0.5)
     axis(4, at = y_ax, labels = F, tck = 0.03)
     axis(2, at = y_ax2, labels = F, las = 1, tck = 0.015, cex.axis = cex.A, adj = 0.5)
@@ -29,11 +29,11 @@ fig.setup <- function(ylim, ylab, y_ax, y_ax2, xlim, xlab, x_ax, x_ax2, cex.A, c
 
 
 to.pdf <- function(expr, filename, ..., verbose = TRUE) {
-    if (!file.exists(dirname(filename))) 
+    if (!file.exists(dirname(filename)))
         dir.create(dirname(filename), recursive = TRUE)
-    if (verbose) 
+    if (verbose)
         cat(sprintf("Creating %s\n", filename))
     pdf(filename, ...)
     on.exit(dev.off())
     eval.parent(substitute(expr))
-} 
+}
